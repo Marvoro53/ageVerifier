2a3f5dbc7285e944b36b2e3b7d7dbabfbf7ab01d
Marvoro53 <marvinlara321@gmail.com>
2020-12-23 16:06:25 -0600

    added a form to the file and some headings

---
+++ app.js
@@ -0,0 +1,86 @@
+var agePrompt=prompt("What is your age?");
+
+if (agePrompt>=18) alert('Welcome!')
+else {
+alert('Sorry! Come back in a few years!')
+document.location="http://www.codelibary.com/";
+
+}
+
+// document.write("Helloe World" + "<br/>");
+// document.write("Helloe World" + "<br/>");
+// document.write("Helloe World" + "<br/>");
+// document.write("Helloe World" + "<br/>");
+// document.write("Helloe World" + "<br/>");
+// document.write("Helloe World" + "<br/>");
+// document.write("Helloe World" + "<br/>");
+// document.write("Helloe World" + "<br/>");
+// document.write("Helloe World" + "<br/>");
+// document.write("Helloe World" + "<br/>");
+
+// for(let i=0;i<10;i++){
+//     document.write("Hello World" + "<br/>");
+// }
+
+// for(i=0;i<10; i++){
+//     if(i===7){
+//         break;
+//     }
+// }
+// document.write("The number is "+i+"<br/>");
+
+// var x=0;
+// while(x<5){
+//     console.log(x);
+//     x=x+1;
+// };
+
+// var x=5;
+// while(x<5){
+//     console.log(x);
+//     x=x+1;
+// };
+
+// for(var i=0;i<20;i++){
+//     if(i % 2===0){
+//         document.write( i + " is even" + "<br>");
+//     }
+//     else{
+//         document.write(i + " is odd")
+//     }
+// } 
+
+// var money=5500.00;
+// var bills=4500.00;
+
+// if (money > bills){
+//     document.write("You are covered!")
+// }
+// else if(money===bills && (money < 5500)){
+//     document.write("Barelyyy covered my dude!")
+// }
+// else{
+//     document.write("uh-oh! no no no..")
+// };
+
+// function assignGrade(abcd){
+    
+//     if(abcd > 90){
+//         return "A";
+//     }
+//     else if(abcd > 80){
+//         return "B";
+//     }
+//     else if(abcd > 70){
+//         return "C";
+//     }
+//     else if(abcd > 60){
+//         return "D"
+//     }
+//     else{
+//         return "F";
+//     }
+// };
+// document.write("You got an " + assignGrade(95) + "<br>")
+// document.write("You got an " + assignGrade(86) + "<br>")
+// document.write("You got an " + assignGrade(45) + "<br>")
